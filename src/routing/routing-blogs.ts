e73bc68b7f87da6ba7c8fff4fd4f3e49f732b816
FoJkee <romanovsky0815@gmail.com>
2023-05-31 14:46:37 +0300

    post all

--- src/routing/routing-blogs.ts
+++ src/routing/routing-blogs.ts
@@ -30,21 +30,67 @@ routingBlogs.post('/', (req: Request, res: Response) => {
     if (errors.length > 0) {
         res.status(400).send({errorsMessages: errors})
         return
-    } else {
-        const blogsPost: blogsType = {
-            id: date.toString(),
-            name: name,
-            description: description,
-            websiteUrl: websiteUrl
-        }
-
-        blogs.push(blogsPost)
-        res.status(201).json(blogsPost)
     }
+
+    const blogsPost: blogsType = {
+        id: date.toString(),
+        name: name,
+        description: description,
+        websiteUrl: websiteUrl
+    }
+    blogs.push(blogsPost)
+    res.status(201).json(blogsPost)
+
 })
 routingBlogs.get('/:id', (req: Request, res: Response) => {
+    const blogsGet = blogs.find(el => el.id === req.params.id)
+    if (!blogsGet) {
+        res.status(404)
+        return
+    }
+    res.status(200).json(blogsGet)
+
 })
-routingBlogs.put('/', (req: Request, res: Response) => {
+routingBlogs.put('/:id', (req: Request, res: Response) => {
+    //Unauthorized????
+
+    const blogsPut = blogs.find(el => el.id === req.params.id)
+
+    if (!blogsPut) {
+        res.status(404)
+        return;
+    }
+
+    const {name, description, websiteUrl} = req.body
+
+    if (!(typeof (name) === "string")
+        || !(typeof (description) === "string")
+        || !(typeof (websiteUrl) === "string")) {
+
+        errors.push({
+            message: "string",
+            field: "string"
+        })
+    }
+    if (errors.length > 0) {
+        res.status(400).send({errorsMessages: errors})
+        return
+    }
+    blogsPut.name = name
+    blogsPut.description = description
+    blogsPut.websiteUrl = websiteUrl
+
+    res.status(204)
+
 })
 routingBlogs.delete('/:id', (req: Request, res: Response) => {
+    //Unauthorized????
+
+    for (let i = 0; i < blogs.length; i++) {
+        if (blogs[i].id === req.params.id) {
+            blogs.splice(i, 1)
+            res.status(204)
+        }
+    }
+    res.status(404)
 })
