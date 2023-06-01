49328c0c96daac20d3bfe9636929a7a9baf641d2
FoJkee <romanovsky0815@gmail.com>
2023-06-01 18:33:28 +0300

    posts complete repositories

--- src/repositories/posts-repositories.ts
+++ src/repositories/posts-repositories.ts
@@ -27,7 +27,7 @@ export const repositoryPosts = {
     },
 
     findPostsId(id: string) {
-        if(id) {
+        if (id) {
             let findGetId = posts.find(el => el.id === id)
             return findGetId
         } else {
@@ -35,12 +35,31 @@ export const repositoryPosts = {
         }
     },
 
+    updatePosts (id: string, title: string, shortDescription: string,
+                 content: string, blogId: string){
 
+        const postsPut = posts.find(el => el.id === id)
 
+        if (postsPut) {
+            postsPut.title = title
+            postsPut.shortDescription = shortDescription
+            postsPut.content = content
+            postsPut.blogId = blogId
+            return true
+        } else {
+            return false
+        }
+    },
 
-
-
-
+    deletePosts(id: string) {
+        for (let i = 0; i < posts.length; i++) {
+            if (posts[i].id === id) {
+                posts.splice(i, 0)
+                return true
+            }
+        }
+        return false
+    }
 
 }
 
--- src/routing/routing-posts.ts
+++ src/routing/routing-posts.ts
@@ -6,72 +6,39 @@ export const routingPosts = Router()
 
 
 routingPosts.get('/', (req: Request, res: Response) => {
+
     const postsGet = repositoryPosts.findPosts()
     res.status(200).json(postsGet)
+
 })
 routingPosts.post('/', (req: Request, res: Response) => {
-
-    // const {id, title, shortDescription, content, blogId, blogName} = req.body
-    //
-    // if (!(typeof (id) === "string") || !(typeof (title) === "string") ||
-    //     !(typeof (shortDescription) === "string") || !(typeof (content) === "string")
-    //     || !(typeof (blogId) === "string") || !(typeof (blogName) === "string")) {
-    //     errors.push({
-    //         message: "string",
-    //         field: "string"
-    //     })
-    // }
-    // if (errors.length > 0) {
-    //     res.status(400).json({errorsMessages: errors})
-    //     return
-    // }
 
     const newPosts = repositoryPosts.createPosts(req.body.title,
         req.body.shortDescription, req.body.content, req.body.blogId,
         req.body.blogName)
     res.status(201).json(newPosts)
 
-
 })
 routingPosts.get('/:id', (req: Request, res: Response) => {
+
     const postsGetId = repositoryPosts.findPostsId(req.params.id)
     postsGetId ? res.status(200).json(postsGetId) : res.status(404)
+
 })
 routingPosts.put('/:id', (req: Request, res: Response) => {
-    const postsPut = posts.find(el => el.id === req.body.id)
-    if (!postsPut) {
-        res.status(404)
-        return
-    }
-    const {id, title, shortDescription, content, blogId, blogName} = req.body
-
-    if (!(typeof (id) === "string") || !(typeof (title) === "string") ||
-        !(typeof (shortDescription) === "string") || !(typeof (content) === "string")
-        || !(typeof (blogId) === "string") || !(typeof (blogName) === "string")) {
-        errors.push({
-            message: "string",
-            field: "string"
-        })
-    }
-    if (errors.length > 0) {
-        res.status(400).json({errorsMessages: errors})
-        return
-    }
-
-    postsPut.title = title
-    postsPut.shortDescription = shortDescription
-    postsPut.content = content
-    postsPut.blogId = blogId
-    res.status(204)
+const putBlogs = repositoryPosts.updatePosts(req.params.id, req.body.title,
+    req.body.shortDescription, req.body.content, req.body.blogId)
+if(putBlogs) {
+   const putBlogsId =  repositoryPosts.findPostsId(req.params.id)
+    res.status(204).send(putBlogsId)
+} else {
+    res.status(404)
+}
 
 })
 routingPosts.delete('/:id', (req: Request, res: Response) => {
-    for (let i = 0; i < posts.length; i++) {
-        if (posts[i].id === req.params.id) {
-            posts.splice(i, 0)
-            res.status(204)
-        }
-    }
-    res.status(404)
+
+    const postDelete = repositoryPosts.deletePosts(req.params.id)
+    postDelete ? res.status(204) : res.status(404)
 
 })
