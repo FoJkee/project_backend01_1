1085addc679b29b3a58f446904e85b23daeafd9d
FoJkee <romanovsky0815@gmail.com>
2023-06-03 19:20:03 +0300

    add middleware1

--- src/middleware/errorsmessages.ts
+++ src/middleware/errorsmessages.ts
@@ -6,15 +6,13 @@ export const errorsMessages = (req: Request, res: Response, next: NextFunction) 
 
 
 
-
-
-
     const result = validationResult(req)
     if (result) {
         res.status(400).json({errorsMessages: result.array()});
     } else {
         next()
     }
+
 
 }
 
