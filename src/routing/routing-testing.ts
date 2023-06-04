334ca94f6349033ec3777c8ccf7e76add00427bd
FoJkee <romanovsky0815@gmail.com>
2023-06-04 10:30:30 +0300

    fix1

--- src/routing/routing-testing.ts
+++ src/routing/routing-testing.ts
@@ -7,13 +7,6 @@ import {blogs} from "../repositories/blogs-repositories";
 export const testingRouter = Router()
 
 testingRouter.delete('/all-data', query('person').notEmpty(), (req: Request, res: Response) => {
-    posts.splice(0)
-
-    if (posts.length > 0) {
-        res.status(204).json([])
-        return
-    }
-
     blogs.splice(0)
     if (blogs.length > 0) {
         res.status(204).json([])
