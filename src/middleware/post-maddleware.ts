2de66a34e6968948165dfbfea3ea8e64e7512621
FoJkee <romanovsky0815@gmail.com>
2023-06-05 15:11:16 +0300

    fix22

--- src/middleware/post-maddleware.ts
+++ src/middleware/post-maddleware.ts
@@ -6,7 +6,7 @@ export const postMaddleware = [
     body('title').exists().bail().isString().bail().trim().isLength({min: 1, max: 30}).withMessage('String length is not more than 30 symbols'),
     body('shortDescription').exists().bail().isString().bail().trim().isLength({min: 1, max: 100}).withMessage('String length is not more than 100 symbols'),
     body('content').exists().bail().isString().bail().trim().isLength({min: 1, max: 1000}).withMessage('String length is not more than 1000 symbols'),
-    body('blogId').exists().bail().isString().bail().trim().isLength({min: 1, max: 100}).withMessage('Incorrect blogId'),
+    body('blogId').exists().bail().isString().bail().isLength({min: 1, max: 100}).withMessage('Incorrect blogId'),
     body('blogName').exists().bail().isString().bail().trim().isLength({min: 1, max: 100}).withMessage('String length is not more than 100 symbols')
 
 ]
