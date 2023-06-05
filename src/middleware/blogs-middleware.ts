c05a35b30902ad5683dd29b5acdc9a8b42949c03
FoJkee <romanovsky0815@gmail.com>
2023-06-05 14:15:00 +0300

    fix16

--- src/middleware/blogs-middleware.ts
+++ src/middleware/blogs-middleware.ts
@@ -2,7 +2,7 @@ import {body} from "express-validator";
 
 
 export const blogsMiddleware = [
-    body('name').exists().bail().isString().trim().isLength({min: 1, max: 15}).withMessage('String length is not more than 15 symbols'),
-    body('description').exists().bail().isString().trim().isLength({min: 1, max: 500}).withMessage('String length is not more than 500 symbols'),
-    body('websiteUrl').exists().isString().trim().isLength({min: 1, max: 100}).withMessage('String length is not more than 100 symbols')
+    body('name').exists().bail().isString().trim().isLength({min: 1, max: 15}).bail().withMessage('String length is not more than 15 symbols'),
+    body('description').exists().bail().isString().trim().isLength({min: 1, max: 500}).bail().withMessage('String length is not more than 500 symbols'),
+    body('websiteUrl').exists().bail().isString().isLength({min: 1, max: 100}).bail().withMessage('String length is not more than 100 symbols').matches(/^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9\-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-\/])*)?/).withMessage('Incorrect websiteUrl')
 ]
