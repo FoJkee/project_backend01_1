825db9b372cf4ac0957799bd1d82bf78e8eb21fd
FoJkee <romanovsky0815@gmail.com>
2023-06-05 14:38:48 +0300

    fix18

--- src/middleware/blogs-middleware.ts
+++ src/middleware/blogs-middleware.ts
@@ -5,5 +5,5 @@ const pattern = /^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za
 export const blogsMiddleware = [
     body('name').exists().bail().isString().trim().isLength({min: 1, max: 15}).bail().withMessage('String length is not more than 15 symbols'),
     body('description').exists().bail().isString().trim().isLength({min: 1, max: 500}).bail().withMessage('String length is not more than 500 symbols'),
-    body('websiteUrl').exists().bail().isString().isLength({min: 1, max: 100}).bail().withMessage('String length is not more than 100 symbols')
+    body('websiteUrl').exists().bail().isString().isLength({min: 1, max: 100}).bail().withMessage('String length is not more than 100 symbols').matches(pattern).withMessage('Incorrect websiteUrl')
 ]
