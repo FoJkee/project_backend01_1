da735f09970e115034c30abe7e817cf372e81ee6
FoJkee <romanovsky0815@gmail.com>
2023-06-05 15:07:43 +0300

    fix21

--- src/middleware/post-maddleware.ts
+++ src/middleware/post-maddleware.ts
@@ -3,10 +3,10 @@ import {body} from "express-validator";
 
 export const postMaddleware = [
 
-    body('title').exists().isString().isLength({min: 1, max: 30}).withMessage('String length is not more than 30 symbols'),
-    body('shortDescription').exists().isString().trim().isLength({min: 1, max: 100}).withMessage('String length is not more than 100 symbols'),
-    body('content').exists().isString().isLength({min: 1, max: 1000}).withMessage('Incorrect content'),
-    body('blogId').exists().isString().trim().isLength({min: 1, max: 100}).withMessage('Incorrect blogId'),
-    body('blogName').exists().isString().isLength({min: 1, max: 15}).withMessage('String length is not more than 15 symbols')
+    body('title').exists().bail().isString().bail().trim().isLength({min: 1, max: 30}).withMessage('String length is not more than 30 symbols'),
+    body('shortDescription').exists().bail().isString().bail().trim().isLength({min: 1, max: 100}).withMessage('String length is not more than 100 symbols'),
+    body('content').exists().bail().isString().bail().trim().isLength({min: 1, max: 1000}).withMessage('String length is not more than 1000 symbols'),
+    body('blogId').exists().bail().isString().bail().trim().isLength({min: 1, max: 100}).withMessage('Incorrect blogId'),
+    body('blogName').exists().bail().isString().bail().trim().isLength({min: 1, max: 100}).withMessage('String length is not more than 100 symbols')
 
 ]
