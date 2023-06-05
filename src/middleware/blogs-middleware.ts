6955f8a41767a6636ecc102398e34f19d7f88031
FoJkee <romanovsky0815@gmail.com>
2023-06-05 14:55:47 +0300

    fix19

--- src/middleware/blogs-middleware.ts
+++ src/middleware/blogs-middleware.ts
@@ -3,7 +3,7 @@ import {body} from "express-validator";
 const pattern = /^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9\-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-\/])*)?/;
 
 export const blogsMiddleware = [
-    body('name').exists().bail().isString().trim().isLength({min: 1, max: 15}).bail().withMessage('String length is not more than 15 symbols'),
-    body('description').exists().bail().isString().trim().isLength({min: 1, max: 500}).bail().withMessage('String length is not more than 500 symbols'),
+    body('name').exists().bail().isString().bail().trim().isLength({min: 1, max: 15}).withMessage('String length is not more than 15 symbols'),
+    body('description').exists().bail().isString().bail().trim().isLength({min: 1, max: 500}).withMessage('String length is not more than 500 symbols'),
     body('websiteUrl').exists().bail().isString().isLength({min: 1, max: 100}).bail().withMessage('String length is not more than 100 symbols').matches(pattern).withMessage('Incorrect websiteUrl')
 ]
