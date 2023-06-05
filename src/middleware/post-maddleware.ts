c27699220bf2679a22370d6cb53e6789a1d51359
FoJkee <romanovsky0815@gmail.com>
2023-06-05 15:34:53 +0300

    fix26

--- src/middleware/post-maddleware.ts
+++ src/middleware/post-maddleware.ts
@@ -7,4 +7,6 @@ export const postMaddleware = [
     body('shortDescription').exists().bail().isString().bail().trim().isLength({min: 1, max: 100}).withMessage('String length is not more than 100 symbols'),
     body('content').exists().bail().isString().bail().trim().isLength({min: 1, max: 1000}).withMessage('String length is not more than 1000 symbols'),
     body('blogId').exists().bail().isString().bail().isLength({min: 1, max: 100}).withMessage('Incorrect blogId'),
+    body('blogName').exists()
+
 ]
